76cbdc0222e01db01b25b1f72b761a22f17b2c6a
peterdinis <petter1@pobox.sk>
2025-01-18 14:19:45 +0100

    infra: Working on landing page WIP

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { NextPage } from "next";
 import Hero from "./_components/hero/Hero";
 import Stats from "./_components/hero/Stats";
+import Categories from "./_components/hero/Categories";
 
 const Homepage: NextPage = () => {
   return (
     <div className="min-h-screen bg-gradient-to-b from-sky-100 to-white">
       <Hero />
       <Stats />
+      <Categories />
     </div>
   )
 }
