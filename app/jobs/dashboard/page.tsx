3f50787a2083b4ddce0147eb16011633b1b65cef
peterdinis <petter1@pobox.sk>
2025-01-25 18:16:56 +0100

    feat: Start working on jobs dashboard page WIP

---
+++ app/jobs/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const JobsDashboardPage: NextPage = () => {
+    return (
+        <>
+            Jobs Dashboard
+        </>
+    )
+}
+
+export default JobsDashboardPage
